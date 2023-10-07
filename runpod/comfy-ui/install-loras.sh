8e324c79690598c99ac5e2220bba22d5fc406bf4
Giacomo Triggiano <giacomotriggiano@gmail.com>
2023-10-07 17:27:31 +0200

    ...

--- runpod/comfy-ui/install-loras.sh
+++ runpod/comfy-ui/install-loras.sh
@@ -22,19 +22,19 @@ if [[ ! -f ${lora_file} ]]; then
 fi
 
 lora_file=${loras_dir}/dungeons_and_dragons_xl_v2.safetensors
-lora_url="https://civitai.com/api/download/models/134343"
+lora_url="https://civitai.com/api/download/models/152042"
 if [[ ! -f ${lora_file} ]]; then
     wget -O ${lora_file} ${lora_url}
 fi
 
 lora_file=${loras_dir}/Aether_Cloud_v1.safetensors
-lora_url="https://civitai.com/api/download/models/141029"
+lora_url="https://civitai.com/api/download/models/156328"
 if [[ ! -f ${lora_file} ]]; then
     wget -O ${lora_file} ${lora_url}
 fi
 
 lora_file=${loras_dir}/lora-sdxl-flaming-person.safetensors
-lora_url="https://civitai.com/api/download/models/119389"
+lora_url="https://civitai.com/api/download/models/129693"
 if [[ ! -f ${lora_file} ]]; then
     wget -O ${lora_file} ${lora_url}
 fi
