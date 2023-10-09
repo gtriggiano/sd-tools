8dffbd070046330450697f267e2635f13707eda5
Giacomo Triggiano <giacomotriggiano@gmail.com>
2023-10-09 23:49:08 +0200

    ...

--- runpod/comfy-ui/install-custom-node/reactor-face-swap.sh
+++ runpod/comfy-ui/install-custom-node/reactor-face-swap.sh
@@ -6,5 +6,7 @@ face_restore_models_dir=/opt/ComfyUI/custom_nodes/comfyui-reactor-node/models/fa
 
 install_custom_node "comfyui-reactor-node" "https://github.com/Gourieff/comfyui-reactor-node"
 
+mkdir -p $face_restore_models_dir
+
 download_if_missing "${face_restore_models_dir}/codeformer.pth" https://github.com/sczhou/CodeFormer/releases/download/v0.1.0/codeformer.pth
 download_if_missing "${face_restore_models_dir}/GFPGANv1.4.pth" https://github.com/TencentARC/GFPGAN/releases/download/v1.3.4/GFPGANv1.4.pth
