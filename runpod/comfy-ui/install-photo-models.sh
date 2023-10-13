9234661f4907fecb3aab53a4838e8fd694b76abd
Giacomo Triggiano <giacomotriggiano@gmail.com>
2023-10-14 00:04:23 +0200

    ...

---
+++ runpod/comfy-ui/install-photo-models.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+bash /sd-tools/runpod/comfy-ui/install-checkpoint/realvis-xl.sh
+bash /sd-tools/runpod/comfy-ui/install-checkpoint/realitycheck-xl.sh
+bash /sd-tools/runpod/comfy-ui/install-checkpoint/juggernaut-xl.sh
