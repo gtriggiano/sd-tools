92d9959ad336d9c685fa861bf17a44b54bfd307a
Giacomo Triggiano <giacomotriggiano@gmail.com>
2025-01-01 18:49:06 +0100

    .

--- runpod/comfy-ui/install-checkpoint/juggernaut-xl.sh
+++ runpod/comfy-ui/install-checkpoint/juggernaut-xl.sh
@@ -2,4 +2,4 @@
 
 source /sd-tools/runpod/comfy-ui/fn.sh
 
-install_checkpoint "juggernautXL_version5.safetensors" "https://civitai.com/api/download/models/166909"
+install_checkpoint "juggernautXL_version5.safetensors" "https://civitai.com/api/download/models/782002?type=Model&format=SafeTensor&size=full&fp=fp16"
--- runpod/comfy-ui/install-checkpoint/realisticvision.sh
+++ runpod/comfy-ui/install-checkpoint/realisticvision.sh
@@ -2,4 +2,4 @@
 
 source /sd-tools/runpod/comfy-ui/fn.sh
 
-install_checkpoint "realisticVisionV51_v51VAE.safetensors" "https://civitai.com/api/download/models/130072?type=Model&format=SafeTensor&size=pruned&fp=fp16"
+install_checkpoint "realisticVisionV51_v51VAE.safetensors" "https://civitai.com/api/download/models/501240?type=Model&format=SafeTensor&size=full&fp=fp16"
